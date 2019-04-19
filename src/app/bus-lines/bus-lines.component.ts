94c1249fa7cc299bcba6e3168a4b19adff3aab33
Felipe <fae.20@hotmail.com>
2019-04-19 18:37:48 -0300

    Implemented structure component bus-lines

--- src/app/bus-lines/bus-lines.component.ts
+++ src/app/bus-lines/bus-lines.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DatapoaService } from '../services/datapoa.service';
+import { Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-bus-lines',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusLinesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dataPoaService: DatapoaService,
+    private formBuilder: FormBuilder
+  ) {
+    this.filter$
+      .pipe(debounceTime(300),
+        map((val) => {
+          this.filterBusLines(val);
+        })
+      )
+      .subscribe();
 
-  ngOnInit() {
+
+      this.busType$
+      .pipe(debounceTime(300),
+        map((val) => {
+          this.getBusType(val);
+        })
+      )
+      .subscribe();
   }
 
+  busLines;
+  busLinesFiltered;
+
+  form: FormGroup;
+
+  filter$ = new Subject<string>();
+  busType$ = new Subject<string>();
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      filter: [""]
+    });
+
+    this.getBusType("onibus");
+  }
+
+  getBusType(type: String){
+    this.form.controls.filter.setValue("");
+    this.busLinesFiltered = [];
+
+    if(type == "onibus")
+      this.getBusLines();
+
+    else
+      this.getBusLinesLotacao();
+  }
+
+  getBusLines(){
+    this.dataPoaService.getBusLines().subscribe((data) => {
+      this.busLines = data;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getBusLinesLotacao(){
+    this.dataPoaService.getBusLinesLotacao().subscribe((data) => {
+      this.busLines = data;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  filterBusLines(filter: String){
+    this.busLinesFiltered = [];
+
+    if(!this.busLines || !filter)
+      return false;
+
+    else{
+      for(let line of this.busLines){
+        if(line.nome.toLowerCase().indexOf(filter.toLowerCase()) != -1)
+          this.busLinesFiltered.push(line);
+      }
+    }
+  }
 }
